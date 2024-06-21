79ed2a9e52f236d2158c4c79f4de59afe1b7a8a0
HildaburJavaDev <hildabur.javadev@gmail.com>
2024-06-21 13:58:05 +0700

    initialize project

---
+++ src/main/java/com/hildabur/bambikbaby/BambikBabyApplication.java
@@ -0,0 +1,13 @@
+package com.hildabur.bambikbaby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambikBabyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BambikBabyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hildabur/bambikbaby/BambikBabyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hildabur.bambikbaby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BambikBabyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
