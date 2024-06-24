7f0803c4d57a99c1f1ca2ba7d404b705f1321e3b
HildaburJavaDev <hildabur.javadev@gmail.com>
2024-06-24 15:17:46 +0700

    refactor UserDetailsServiceImpl for implements ExtendedUserDetailsService

--- src/main/java/com/hildabur/bambikbaby/services/UserDetailsServiceImpl.java
+++ src/main/java/com/hildabur/bambikbaby/services/UserDetailsServiceImpl.java
@@ -1,24 +1,28 @@
 package com.hildabur.bambikbaby.services;
 
 import com.hildabur.bambikbaby.dao.UserRepository;
+import com.hildabur.bambikbaby.exceptions.UserNotFoundException;
 import com.hildabur.bambikbaby.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService {
-    @Autowired
+public class UserDetailsServiceImpl implements ExtendedUserDetailsService {
     private UserRepository userRepository;
 
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String phoneNumber) throws UsernameNotFoundException {
-        User user = userRepository.findByPhoneNumber(phoneNumber)
-                .orElseThrow(() -> new UsernameNotFoundException(
-                        String.format("Пользователь с номером телефона %s не найден", phoneNumber))
-                );
+    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException { return null; }
+
+    public UserDetails loadUserById(Long id) throws UserNotFoundException {
+        User user = userRepository.findUserById(id)
+                .orElseThrow(() -> new UserNotFoundException( "Пользователь не найден"));
         return new UserDetailsImpl(
                 (long) user.getId(),
                 user.getPhoneNumber(),
