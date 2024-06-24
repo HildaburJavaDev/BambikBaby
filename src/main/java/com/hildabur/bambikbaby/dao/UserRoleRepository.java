75f0f41a8aa143fab5699977542c1a7c7e9e9b00
HildaburJavaDev <hildabur.javadev@gmail.com>
2024-06-24 18:24:21 +0700

    add UserRoleRepository

---
+++ src/main/java/com/hildabur/bambikbaby/dao/UserRoleRepository.java
@@ -0,0 +1,8 @@
+package com.hildabur.bambikbaby.dao;
+
+import com.hildabur.bambikbaby.models.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+    UserRole findByName(String name);
+}
