8d6a1041f3da58a4525fce88db03c77de19daa3f
HildaburJavaDev <hildabur.javadev@gmail.com>
2024-06-24 17:51:35 +0700

    add AuthController

---
+++ src/main/java/com/hildabur/bambikbaby/controllers/AuthController.java
@@ -0,0 +1,10 @@
+package com.hildabur.bambikbaby.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@PostMapping("/auth")
+public class AuthController {
+
+}
