faac42728f712135497113b431625145b3db0488
HildaburJavaDev <hildabur.javadev@gmail.com>
2024-06-24 23:09:50 +0700

    make correct methods to find models JPA

--- src/main/java/com/hildabur/bambikbaby/dao/UserRepository.java
+++ src/main/java/com/hildabur/bambikbaby/dao/UserRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByPhoneNumber(String phoneNumber);
+    boolean existsByPhoneNumber(String phoneNumber);
     Optional<User> findUserById(Long id);
 }
