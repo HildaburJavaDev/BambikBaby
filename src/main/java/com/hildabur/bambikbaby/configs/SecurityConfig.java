0610fb084aaac596313fe0ea210c6775aa7b2580
HildaburJavaDev <hildabur.javadev@gmail.com>
2024-06-24 23:08:35 +0700

    update Secutiry to permit all

--- src/main/java/com/hildabur/bambikbaby/configs/SecurityConfig.java
+++ src/main/java/com/hildabur/bambikbaby/configs/SecurityConfig.java
@@ -1,4 +1,69 @@
 package com.hildabur.bambikbaby.configs;
 
-public class SecurityConfig{
+import com.hildabur.bambikbaby.services.TokenFilter;
+import com.hildabur.bambikbaby.services.UserDetailsServiceImpl;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
+@Data
+public class SecurityConfig {
+
+    private TokenFilter tokenFilter;
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Autowired
+    public void setTokenFilter(TokenFilter tokenFilter) {
+        this.tokenFilter = tokenFilter;
+    }
+
+    @Autowired
+    public SecurityConfig(TokenFilter tokenFilter, UserDetailsServiceImpl userDetailsServiceImpl) {
+        this.tokenFilter = tokenFilter;
+        this.userDetailsServiceImpl = userDetailsServiceImpl;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(httpSecurityCorsConfigurer ->
+                        httpSecurityCorsConfigurer.configurationSource(request ->
+                                new CorsConfiguration().applyPermitDefaultValues()))
+                .exceptionHandling(exceptions -> exceptions
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/auth/**").permitAll()
+//                        .requestMatchers("/secured/user").fullyAuthenticated()
+//                        .anyRequest().fullyAuthenticated())
+                .addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
